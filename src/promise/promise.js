5a18a54c3efe1cf26f425858bb1154e102d4a913
JackLiR8 <jiakeli1024@gmail.com>
2021-03-30 15:41:16 +0800

    refactor(promise): refactor `Promise.race()` and `Promise.all()`

--- src/promise/promise.js
+++ src/promise/promise.js
@@ -18,9 +18,6 @@ export function MyPromise(callback) {
   defineValue(this, 'PromiseState', STATUS.PENDING)
   defineValue(this, 'PromiseResult', undefined)
 
-  defineValue(this, 'handlers_then', [])
-  defineValue(this, 'handlers_catch', [])
-
   const _resolve = (value) => {
     this.PromiseResult = value
     this.PromiseState = STATUS.FULFILLED
@@ -40,14 +37,11 @@ export function MyPromise(callback) {
 /* ====================================================== */
 
 MyPromise.prototype.then = (fulfillHandler, rejectHandler) => {
-  if (fulfillHandler) this.handlers_then.push(fulfillHandler)
-  if (rejectHandler) this.handlers_catch.push(rejectHandler)
-  return this
+  // TODO then
 }
 
 MyPromise.prototype.catch = (rejectHandler) => {
-  if (rejectHandler) this.handlers_catch.push(rejectHandler)
-  return this
+  // TODO catch
 }
 
 
@@ -62,7 +56,7 @@ MyPromise.all = (promises) => {
 
   return new Promise((resolve, reject) => {
     for (let i = 0, promise = promises[0]; i < len; promise = promises[++i]) {
-      promise
+      Promise.resolve(promise)
         .then(res => {
           fulfilled++
           results[i] = res
@@ -78,17 +72,11 @@ MyPromise.all = (promises) => {
 
 
 MyPromise.race = (promises) => {
-  let fulfilled = false
   return new Promise((resolve, reject) => {
     for (const promise of promises) {
-      promise
-        .then(res => {
-          if (!fulfilled) {
-            fulfilled = true
-            resolve(res)
-          }
-        })
-        .catch(err => reject(err))
+      Promise.resolve(promise)
+        .then(resolve)
+        .catch(reject)
     }
   })
 }
