f41f46977653af6867716a8ff55a9e9196318109
JackLiR8 <jiakeli1024@gmail.com>
2021-07-16 14:47:02 +0800

    feat: Object

--- src/learning/object/content.js
+++ src/learning/object/content.js
@@ -27,3 +27,27 @@ myObject["[object Object]"]; // "baz"
 const arr = ['foo', 1, 'baz']
 arr['10'] = 'bar'
 console.log(arr.length) // 11
+
+
+/* ------------------------- 不变性 ------------------------ */
+const imObj = {}
+// 1. 常量属性
+Object.defineProperty(imObj, 'a', {
+  value: 1,
+  writable: false,
+  configurable: false
+})
+
+// 2. 禁止扩展
+Object.preventExtensions(imObj)
+imObj.b = 2
+console.log(imObj.b)  // undefined
+
+// 3. Object.seal()
+// 在一个现有对象上调用 Object.preventExtensions(..) 
+// 并把所有现有属性标记为 configurable:false
+// 密封之后不可以新增属性，不可修改现有属性配置，但是可以修改现有属性
+
+// 4. Object.freeze()
+// 这个方法实际上会在一个现有对象上调用 Object.seal(..) 并把所有“数据访问”属性
+// 标记为 writable:false，这样就无法修改它们的值
