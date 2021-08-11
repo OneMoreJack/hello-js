52718689660bafd609d6ca65e1f093178920e391
JackLiR8 <jiakeli1024@gmail.com>
2021-08-11 09:09:36 +0800

    feat(algorithms): 新增选择排序

--- src/algorithms/sort/__test__/sort.spec.js
+++ src/algorithms/sort/__test__/sort.spec.js
@@ -1,4 +1,4 @@
-const { bubble } = require('../sort')
+const { bubble, select } = require('../sort')
 
 describe('冒泡排序', () => {
   it('用例1', () => {
@@ -7,5 +7,16 @@ describe('冒泡排序', () => {
 
   it('用例2', () => {
     expect(bubble([1, 0, 9, 12, 4, 9, 6])).toEqual([0, 1, 4, 6, 9, 9, 12])
+  })
+})
+
+
+describe('选择排序', () => {
+  it('用例1', () => {
+    expect(select([9, 5, 2, 8, 1, 7])).toEqual([1, 2, 5, 7, 8, 9])
+  })
+
+  it('用例2', () => {
+    expect(select([1, 0, 9, 12, 4, 9, 6])).toEqual([0, 1, 4, 6, 9, 9, 12])
   })
 })
--- src/algorithms/sort/sort.js
+++ src/algorithms/sort/sort.js
@@ -16,6 +16,31 @@ function bubble(nums) {
   return nums
 }
 
+/**
+ * 选择排序
+ * 时间复杂度 O(n^2)
+ * 空间复杂度 O(1)
+ * @param {Array} nums 
+ * @returns {Array}
+ */
+function select(nums) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    let curr = nums[i],
+        minIdx = i
+    
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] < nums[minIdx]) {
+        minIdx = j
+      }
+    }
+    
+    nums[i] = nums[minIdx]
+    nums[minIdx] = curr
+  }
+  return nums
+}
+
 module.exports = {
-  bubble
+  bubble,
+  select,
 }
