ac34b7bcb69cbd570f8a960d2efa24ce707e6534
JackLiR8 <jiakeli1024@gmail.com>
2021-09-18 10:19:39 +0800

    chore(jest): 配置测试覆盖率收集范围

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/simulating/**/*.js',
+    'src/algorithms/**/*.js',
   ],
   coverageReporters: ['html', 'text'],
 }
