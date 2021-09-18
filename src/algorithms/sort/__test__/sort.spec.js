61b3cc3f122ce812a8c89681653acffd81466f2d
JackLiR8 <jiakeli1024@gmail.com>
2021-09-18 10:20:54 +0800

    refactor: 排序测试文件优化

--- src/algorithms/sort/__test__/sort.spec.js
+++ src/algorithms/sort/__test__/sort.spec.js
@@ -1,52 +1,40 @@
 const { bubble, select, insert, heapSort, mergeSort } = require('../sort')
 
-describe('冒泡排序', () => {
-  it('用例1', () => {
-    expect(bubble([9, 5, 2, 8, 1, 7])).toEqual([1, 2, 5, 7, 8, 9])
-  })
+const test1 = () => {
+  expect(bubble([9, 5, 2, 8, 1, 7])).toEqual([1, 2, 5, 7, 8, 9])
+}
 
-  it('用例2', () => {
-    expect(bubble([1, 0, 9, 12, 4, 9, 6])).toEqual([0, 1, 4, 6, 9, 9, 12])
-  })
+const test2 = () => {
+  expect(bubble([1, 0, 9, 12, 4, 9, 6])).toEqual([0, 1, 4, 6, 9, 9, 12])
+}
+
+describe('冒泡排序', () => {
+  it('用例1', test1)
+  it('用例2', test2)
 })
 
 
 describe('选择排序', () => {
-  it('用例1', () => {
-    expect(select([9, 5, 2, 8, 1, 7])).toEqual([1, 2, 5, 7, 8, 9])
-  })
-
-  it('用例2', () => {
-    expect(select([1, 0, 9, 12, 4, 9, 6])).toEqual([0, 1, 4, 6, 9, 9, 12])
-  })
+  it('用例1', test1)
+  it('用例2', test2)
 })
 
 describe('插入排序', () => {
-  it('用例1', () => {
-    expect(insert([9, 5, 2, 8, 1, 7])).toEqual([1, 2, 5, 7, 8, 9])
-  })
-
-  it('用例2', () => {
-    expect(insert([1, 0, 9, 12, 4, 9, 6])).toEqual([0, 1, 4, 6, 9, 9, 12])
-  })
+  it('用例1', test1)
+  it('用例2', test2)
 })
 
 describe('堆排序', () => {
-  it('用例1', () => {
-    expect(heapSort([9, 5, 2, 8, 1, 7])).toEqual([1, 2, 5, 7, 8, 9])
-  })
-
-  it('用例2', () => {
-    expect(heapSort([1, 0, 9, 12, 4, 9, 6])).toEqual([0, 1, 4, 6, 9, 9, 12])
-  })
+  it('用例1', test1)
+  it('用例2', test2)
 })
 
 describe('归并排序', () => {
-  it('用例1', () => {
-    expect(mergeSort([9, 5, 2, 8, 1, 7])).toEqual([1, 2, 5, 7, 8, 9])
-  })
+  it('用例1', test1)
+  it('用例2', test2)
+})
 
-  it('用例2', () => {
-    expect(mergeSort([1, 0, 9, 12, 4, 9, 6])).toEqual([0, 1, 4, 6, 9, 9, 12])
-  })
+describe('希尔排序', () => {
+  it('用例1', test1)
+  it('用例2', test2)
 })
