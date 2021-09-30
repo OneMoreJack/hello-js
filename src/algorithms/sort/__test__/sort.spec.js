7f31702bd5784f4fef46e400f7c05b804e3a93f5
JackLiR8 <jiakeli1024@gmail.com>
2021-09-30 08:59:38 +0800

    refactor(test): 排序单元测试重构

--- src/algorithms/sort/__test__/sort.spec.js
+++ src/algorithms/sort/__test__/sort.spec.js
@@ -1,49 +1,20 @@
 const { bubble, select, insert, heapSort, mergeSort, hellSort, countingSort } = require('../sort')
 
-const test1 = func => {
+const getTestFunc = func => {
   return () => {
     expect(func([9, 5, 2, 8, 1, 7])).toEqual([1, 2, 5, 7, 8, 9])
-  }
-}
-
-const test2 = func => {
-  return () => {
     expect(func([1, 0, 9, 12, 4, 9, 6])).toEqual([0, 1, 4, 6, 9, 9, 12])
+    expect(func([13, 24, 0, 6, 88, 2, 36])).toEqual([0, 2, 6, 13, 24, 36, 88])
+    expect(func([2, 1, 0, 1])).toEqual([0, 1, 1, 2])
   }
 }
 
-describe('冒泡排序', () => {
-  it('用例1', test1(bubble))
-  it('用例2', test2(bubble))
-})
-
-
-describe('选择排序', () => {
-  it('用例1', test1(select))
-  it('用例2', test2(select))
-})
-
-describe('插入排序', () => {
-  it('用例1', test1(insert))
-  it('用例2', test2(insert))
-})
-
-describe('堆排序', () => {
-  it('用例1', test1(heapSort))
-  it('用例2', test2(heapSort))
-})
-
-describe('归并排序', () => {
-  it('用例1', test1(mergeSort))
-  it('用例2', test2(mergeSort))
-})
-
-describe('希尔排序', () => {
-  it('用例1', test1(hellSort))
-  it('用例2', test2(hellSort))
-})
-
-describe('计数排序', () => {
-  it('用例1', test1(countingSort))
-  it('用例2', test2(countingSort))
+describe('排序', () => {
+  it('冒泡排序', getTestFunc(bubble))
+  it('选择排序', getTestFunc(select))
+  it('插入排序', getTestFunc(insert))
+  it('堆排序', getTestFunc(heapSort))
+  it('归并排序', getTestFunc(mergeSort))
+  it('希尔排序', getTestFunc(hellSort))
+  it('计数排序', getTestFunc(countingSort))
 })
