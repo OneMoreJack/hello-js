2f179ed31017a41578d24f0b50296f3198408b00
JackLiR8 <jiakeli1024@gmail.com>
2021-10-13 10:12:55 +0800

    feat(queue): 新增 queue 的链表实现

--- src/algorithms/queue/__test__/queue.spec.ts
+++ src/algorithms/queue/__test__/queue.spec.ts
@@ -1,9 +1,33 @@
-const { sum }  = require('../queue')
+import { Queue } from '../queue'
 
 describe('Queue', () => {
-  it('sum', () => {
-    expect(sum(1, 2)).toBe(3)
-    expect(sum(2, 4)).toBe(6)
+  test('enqueue, dequeue, isEmpty, size', () => {
+    const queue = new Queue<number>()
+    expect(queue.isEmpty()).toBe(true)
+
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    expect(queue.isEmpty()).toBe(false)
+    expect(queue.size()).toBe(3)
+
+    const item = queue.dequeue()
+    expect(item).toBe(1)
+    expect(queue.size()).toBe(2)
+  })
+
+  test('iterable', () => {
+    let temp: number[] = []
+    const queue = new Queue<number>()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    queue.enqueue(4)
+    queue.enqueue(5)
+    for (let item  of queue) {
+      temp.push(item as number)
+    }
+    expect(temp).toEqual([1, 2, 3, 4, 5])
   })
 })
 
--- src/algorithms/queue/queue.ts
+++ src/algorithms/queue/queue.ts
@@ -1,4 +1,56 @@
 
-export function sum(a: number, b: number) {
-  return a + b
+class Node<T> {
+  value: T | undefined = undefined
+  next: Node<T> | null = null
+}
+
+export class Queue<T> {
+  private first: Node<T> | null = null
+  private last: Node<T> | null = null
+  private N: number = 0
+
+  public isEmpty(): boolean {
+    return this.N === 0
+  }
+
+  public size(): number {
+    return this.N
+  }
+
+  public enqueue(item: T) {
+    let oldLast = this.last
+    let last = new Node<T>()
+    last.value = item
+    last.next = null
+    this.last = last
+
+    if (this.isEmpty()) this.first = this.last
+    else (oldLast as Node<T>).next = this.last
+    this.N++
+  }
+
+  public dequeue(): T | undefined {
+    if (!this.first) return undefined
+
+    const item = this.first.value
+    this.first = this.first?.next
+    this.N--
+    if (this.isEmpty()) this.last = null
+    return item
+  }
+
+  public [Symbol.iterator]() {
+    let node = this.first
+    return {
+      next: () => {
+        let iterateObj = {
+          value: node == null ? undefined : node.value,
+          done: node == null
+        }
+
+        if (node) node = node.next
+        return iterateObj
+      }
+    }
+  }
 }
