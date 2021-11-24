d338e2d4ad4a9854b5bf0fe1a0e772a4b3684c13
JackLiR8 <jiakeli1024@gmail.com>
2021-11-24 10:33:40 +0800

    chore(jest): 默认不生成覆盖率报告

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  collectCoverage: true,
+  collectCoverage: false,
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/simulating/**/*.[tj]s',
